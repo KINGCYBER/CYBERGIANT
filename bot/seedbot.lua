b94199ab332c9758af5461ab0630b6884409937d
KINGCYBER <amir.faramarzi.af.1380@gmail.com>
2016-10-17 18:55:30 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {120741730,tonumber(our_id)},--Sudo users
+    sudo_users = {120741730},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
